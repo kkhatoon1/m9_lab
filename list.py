d2061a96538829b3de3213b616a989c317fbc24b
kkhatoon1 <kkhatoon1@my.nl.edu>
2024-11-19 14:14:03 -0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+#khateeja khatoon
+# nov 19 2024
+# probelm 2:create a list with initials.
+
+#initialize the counter
+L = [57,83]
+counter = 0 
+
+while counter <= 10:
+    L.append(counter)
+    counter +=1
+
+print(f"the list has{len} elements.")
+print(f"the third element in the list is{L[2]}")
